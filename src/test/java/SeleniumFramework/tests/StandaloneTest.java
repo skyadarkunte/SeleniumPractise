501f20fedc20dee1dde39e002c86ddb1ca2f40e1
Sandeep <skyadarkunte@gmail.com>
2024-06-09 17:35:07 +0530

    Updates Standalone TestFile

--- src/test/java/SeleniumFramework/tests/StandaloneTest.java
+++ src/test/java/SeleniumFramework/tests/StandaloneTest.java
@@ -3,7 +3,7 @@ package SeleniumFramework.tests;
 
 import java.time.Duration;
 import java.util.List;
-
+//Test
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
